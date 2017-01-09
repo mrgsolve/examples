c3cb3a83076fd3ca5b3d4d2a9d1c1ac0ced59199
Kyle Baron <kyleb@metrumrg.com>
2017-01-09 13:25:07 -0600

    adding approx example

---
+++ misc_demo/approx.R
@@ -0,0 +1,82 @@
+##' ---
+##' output: 
+##'   md_document:
+##'     variant: markdown_github
+##' ---
+#+ message=FALSE
+
+library(dplyr)
+library(mrgsolve)
+library(magrittr)
+library(ggplot2)
+
+#+ echo=FALSE
+library(knitr)
+opts_chunk$set(fig.path="img/covset-",comment='.')
+
+##' # About the code
+##' 
+##' - Use `$PLUGIN`
+##'     - `Rcpp`
+##'     - `mrgx`
+##' - We declare some variables in `$GLOBAL` so we can use them in any part of the model
+##'     - `appx` is an `R` function; assign it to `mrgx::mt_fun()` to hold the place
+##'     - `x` will be the `x` argument to `stats::approx`
+##'     - `y` will be the `y` argument to `stats::approx`
+##' - `xout` enters as a parameter (we can change it); that also gets passed to `stats::approx`
+##' - `$PREAMBLE` gets called __ONCE__ we set thing up there
+##'     - First, `mrgx::get` the `approx` function from the `stats` namespace
+##'     - Note that the call is `mrgx::get<Rcpp::Function>`; `mrgx::get` is a templated function, so we need to 
+##'     say what type we are working with.
+##'     - We also `mrgx::get` some `Rcpp::NumericVector`s (`x` and `y`) from the model `$ENV`
+##' - Now, in `$MAIN` we can call the `appx` function and pass in `x`, `y`, and `xout`.
+##'     - `appx` returns the result as a `Rcpp::List`.  We're interested in the `y` element in that list, 
+##'     so we need to get specific about what type (`double`) that needs to be.
+##' 
+##' # More info
+##' 
+##' - http://mrgsolve.github.io/user_guide/model-specification.html#block-plugin
+##' 
+##' 
+
+
+code <- '
+$PARAM xout = 13
+
+$PLUGIN Rcpp mrgx
+
+$GLOBAL 
+Rcpp::Function appx = mrgx::mt_fun(); 
+Rcpp::NumericVector x;
+Rcpp::NumericVector y;
+
+$PREAMBLE
+appx = mrgx::get<Rcpp::Function>("stats", "approx");
+x = mrgx::get<Rcpp::NumericVector>("x", self);
+y = mrgx::get<Rcpp::NumericVector>("y", self);
+
+$MAIN
+Rcpp::List out = appx(x,y,xout);
+double yout = Rcpp::as<double>(out["y"]);
+
+$ENV
+set.seed(11122)
+n <- 10
+x <- sort(runif(n,10,20))
+y <- sort(rnorm(n))
+
+$CAPTURE xout yout
+'
+
+#+
+mod <- mcode("approx", code)
+
+#+
+e <- get_env(mod)
+
+#+
+approx(e$x,e$y, xout=13)
+
+#+
+mrgsim(mod, end=-1) %>% as.data.frame
+
